bbacee71c64f324193fb1beb4f0ab45cacc07ad5
ruchira <htruchira@gmail.com>
2019-05-02 11:04:45 +0530

    Initial commit

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController :Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contacts";
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            ViewBag.Title = "AboutUs";
+            return View();
+        }
+    }
+}
---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+
+    var x = 10;
+    var y = "";
+
+
+    console.log("Hello world");
+
+    var theForm = $("#backGroundForm");
+    theForm.hide();
+
+    var item = document.getElementById("buyItem");
+    item.addEventListener("click", function () {
+        console.log("Buying the item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You have clicked on" + $(this).text());
+    });
+
+
+    var $testLogin = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $testLogin.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
+});
+
+
+
